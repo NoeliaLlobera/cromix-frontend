6dfd525a5a1909c82e444709c4acdfcf2d5d71cd
Noèlia Llobera Sànchez <noelialloberas@gmail.com>
2024-12-07 15:24:07 +0100

    Creation of project and dependency and styles config.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, NgbModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'cromix-frontend';
