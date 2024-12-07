fedc599870684f5c8faaafa5997aa76e63949ea5
Noèlia Llobera Sànchez <noelialloberas@gmail.com>
2024-12-07 22:26:51 +0100

    landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import {TranslateDirective, TranslatePipe, TranslateService} from "@ngx-translate/core";
+import {HeaderComponent} from "./shared/header/header.component";
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, NgbModule, TranslatePipe, TranslateDirective],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [RouterOutlet, NgbModule, TranslatePipe, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {LandingComponent} from "./landing/views/landing/landing.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent,
+    title: 'Landing',
+    data: {}
+  }
+];
+
+
+// const routes: Routes = [
+//   {
+//     path: 'lazy',
+//     loadComponent: () => import('./lazy.component').then(c => c.LazyComponent)
+//   }
+// ];
