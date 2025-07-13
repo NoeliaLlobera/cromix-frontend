8602952281c772a345a051f2140bb5fecee70dee
Noèlia Llobera Sànchez <noelialloberas@gmail.com>
2025-07-13 19:28:16 +0200

    finish ngrx integration

--- src/app/edit-collection/services/edit-collection.service.ts
+++ src/app/edit-collection/services/edit-collection.service.ts
@@ -1,5 +1,5 @@
 import {inject, Injectable} from '@angular/core';
-import {lastValueFrom} from "rxjs";
+import {lastValueFrom, Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {COLLECTIONS_ENDPOINTS, CROMO_TYPES_ENDPOINTS} from "../../core/constants/api";
 import {CollectionDTO} from "../../core/models/collectionDTO";
@@ -11,16 +11,23 @@ import {CromoTypeDTO} from "../../core/models/cromo-typeDTO";
 export class EditCollectionService {
   private readonly http: HttpClient = inject(HttpClient);
 
-
-  async getCollectionById(id: string): Promise<CollectionDTO[]> {
-    const response: any = await lastValueFrom(this.http.get(COLLECTIONS_ENDPOINTS.GET_BY_ID(id)));
-
-    return response
+  getCollectionById(id: string): Observable<CollectionDTO> {
+    return this.http.get<CollectionDTO>(COLLECTIONS_ENDPOINTS.GET_BY_ID(id));
   }
 
-  async getCromoTypeByCollectionId(id: string): Promise<CromoTypeDTO[]> {
-    const response: any = await lastValueFrom(this.http.get(CROMO_TYPES_ENDPOINTS.GET_BY_COLLECTION_ID(id)));
+  getCromoTypeByCollectionId(id: string): Observable<CromoTypeDTO[]> {
+    return this.http.get<CromoTypeDTO[]>(CROMO_TYPES_ENDPOINTS.GET_BY_COLLECTION_ID(id));
+  }
 
-    return response;
+  createCromo(cromo: CromoTypeDTO): Observable<CromoTypeDTO> {
+    return this.http.post<CromoTypeDTO>(CROMO_TYPES_ENDPOINTS.CREATE, cromo);
+  }
+
+  updateCromo(cromo: CromoTypeDTO): Observable<CromoTypeDTO> {
+    return this.http.patch<CromoTypeDTO>(CROMO_TYPES_ENDPOINTS.UPDATE(cromo.id), cromo);
+  }
+
+  deleteCromo(cromoId: string): Observable<void> {
+    return this.http.delete<void>(CROMO_TYPES_ENDPOINTS.DELETE(cromoId));
   }
 }
--- src/app/edit-collection/views/print-page/print-page.component.ts
+++ src/app/edit-collection/views/print-page/print-page.component.ts
@@ -4,33 +4,41 @@ import {CromoTypeDTO} from "../../../core/models/cromo-typeDTO";
 import {ActivatedRoute} from "@angular/router";
 import {ConfigService} from "../../../core/services/config.service";
 import {REVERSE_IMAGE} from "../../../core/constants";
+import {Observable} from "rxjs";
+import {Store} from "@ngrx/store";
+import {selectCromoTupes} from "../../../store/cromos/cromos.selectors";
+import {AsyncPipe} from "@angular/common";
+import {getCromos} from "../../../store/cromos/cromos.actions";
 
 @Component({
   selector: 'app-print-page',
-  imports: [],
+  imports: [
+    AsyncPipe
+  ],
   templateUrl: './print-page.component.html',
   styleUrl: './print-page.component.scss'
 })
 export class PrintPageComponent implements OnInit, OnDestroy {
-  private readonly service: EditCollectionService = inject(EditCollectionService);
   private readonly route: ActivatedRoute = inject(ActivatedRoute);
   private readonly configService: ConfigService = inject(ConfigService);
+  private readonly store = inject(Store);
 
-  cromoTypeData: CromoTypeDTO[] = [];
+  cromoTypeData$!: Observable<CromoTypeDTO[]>;
 
   constructor() {
     this.configService.hasHeader.set(false);
   }
 
   async ngOnInit() {
-    this.cromoTypeData = await this.service.getCromoTypeByCollectionId(this.route.snapshot.params['id']);
+    this.store.dispatch(getCromos({collection_id: this.route.snapshot.params['id']}));
+    // this.cromoTypeData = await this.service.getCromoTypeByCollectionId(this.route.snapshot.params['id']);
     // open print dialog
+    this.cromoTypeData$ = this.store.select(selectCromoTupes);
     setTimeout(() => {
       window.print();
     }, 500);
 
   }
-
 
 
   async ngOnDestroy() {
@@ -39,5 +47,5 @@ export class PrintPageComponent implements OnInit, OnDestroy {
   }
 
 
-    protected readonly REVERSE_IMAGE = REVERSE_IMAGE;
+  protected readonly REVERSE_IMAGE = REVERSE_IMAGE;
 }
