a376b1e18638e6f7692947b3eaf85ac925d8fe7d
NoeliaLlobera <noelia.llobera@sdp.biz>
2026-01-07 18:23:13 +0100

    Progress

--- src/app/cards-list/views/preview-cards/cards-list.component.ts
+++ src/app/cards-list/views/preview-cards/cards-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnInit, signal, WritableSignal} from '@angular/core';
+import {Component, inject, OnDestroy, OnInit, signal, WritableSignal} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {TranslatePipe} from "@ngx-translate/core";
 import {CardsListService} from "./service/cards-list.service";
@@ -6,17 +6,19 @@ import {LoaderComponent} from "../../../shared/components/loader/loader.componen
 import {CollectionDTO} from "../../../core/models/collectionDTO";
 import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {ModalComponent} from "./components/modal/modal.component";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-preview-cards',
   imports: [
     TranslatePipe,
     LoaderComponent,
+    JsonPipe,
   ],
   templateUrl: './cards-list.component.html',
   styleUrl: './cards-list.component.scss'
 })
-export class CardsListComponent implements OnInit {
+export class CardsListComponent implements OnInit, OnDestroy {
   protected route: ActivatedRoute = inject(ActivatedRoute);
   private readonly router: Router = inject(Router);
   cromos: WritableSignal<any[] | null> = signal(null);
@@ -26,23 +28,63 @@ export class CardsListComponent implements OnInit {
   private originalCromos: any[] = [];
 
 
-
   private readonly service: CardsListService = inject(CardsListService);
   private modalService: NgbModal = inject(NgbModal);
 
   protected packAvailable: boolean = true;
-   hideRepseated: any = false;
-   uniqueCromos!: any[];
+  hideRepseated: any = false;
+  uniqueCromos!: any[];
+  private intervalId!: number;
+  private secondsToNextPack!: number;
+  countdown: { hours: number; minutes: number; seconds: number } | null = null;
 
 
   ngOnInit() {
     this.getCromos().then()
     this.getCollection().then();
 
+
     if (this.route.snapshot.url[0].path) {
       this.mode = this.route.snapshot.url[0].path;
     } else {
       this.mode = 'preview';
+    }
+
+    this.intervalId = window.setInterval(() => {
+      this.checkPackAvailability();
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    // Limpia el intervalo cuando el componente se destruye
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  private checkPackAvailability() {
+    // Tu lógica aquí (se ejecutará cada segundo)
+    console.log('Checking pack availability...');
+    // Ejemplo: actualizar packAvailable según alguna condición
+
+    //comprovar cuantos segundos faltan para que this.originalCromos[0].created_at + this.collection.periodicity teniendo en cuenta que periodicity són horas
+    if (this.originalCromos.length > 0) {
+      const lastPackTime = new Date(this.originalCromos[0].created_at).getTime();
+      const nowUTC = new Date().toISOString();
+      const now = new Date(nowUTC).getTime();
+      const periodicityInMs = this.collection!.periodicity! * 60 * 60 * 1000;
+      this.packAvailable = now >= (lastPackTime + periodicityInMs);
+      this.secondsToNextPack = Math.max(0, Math.floor((lastPackTime + periodicityInMs - now) / 1000));
+      if(this.packAvailable) {
+        this.countdown = null;
+        return;
+      }
+      this.countdown = {
+        hours: Math.floor(this.secondsToNextPack / 3600),
+        minutes: Math.floor((this.secondsToNextPack % 3600) / 60),
+        seconds: this.secondsToNextPack % 60
+      };
+
     }
   }
 
